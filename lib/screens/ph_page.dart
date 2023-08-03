e08ce92f20af6c48596ae08cef1cf52122f212e8
Vince Gelo Buala <vdbuala@up.edu.ph>
2023-08-03 23:47:22 +0800

    Added initial setup for line graph

--- lib/screens/ph_page.dart
+++ lib/screens/ph_page.dart
@@ -13,6 +13,17 @@ import 'package:week7_networking_discussion/screens/modal_todo.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:firebase_database/firebase_database.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:intl/intl.dart';
+
+class SensorData {
+  final double waterTemperature;
+  final double pH;
+  final int timestamp;
+  final String timeUpload;
+
+  SensorData(this.waterTemperature, this.pH, this.timestamp, this.timeUpload);
+}
 
 class PH_Page extends StatefulWidget {
   const PH_Page({super.key});
@@ -22,6 +33,100 @@ class PH_Page extends StatefulWidget {
 }
 
 class _PHPageState extends State<PH_Page> {
+  List<SensorData> dataList = [];
+  var phVal = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _subscribeToSensorData();
+  }
+
+  void _subscribeToSensorData() async {
+    DateTime current_date = DateTime.now();
+    String date_today = current_date.toString().split(' ')[0];
+
+    print(date_today);
+    //firebase realtime database reference
+    final ref = FirebaseDatabase(
+        databaseURL:
+            "https://sp2-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
+    //listens to any changes at the data_sensor/$date_today and consequently updates the
+    void streamSubscription = await ref
+        .reference()
+        .child("$date_today")
+        .onValue
+        .listen((DatabaseEvent event) {
+      setState(() {
+        print(event.snapshot.value);
+        Map<dynamic, dynamic> sensorDataMap =
+            event.snapshot.value as Map<dynamic, dynamic>;
+        print("here");
+        if (sensorDataMap != null) {
+          sensorDataMap.forEach((key, value) {
+            if (value != null) {
+              var pH = value["ph"].toDouble();
+              var waterTemperature = value["water_temperature"].toDouble();
+
+              var timestamp =
+                  int.parse(key); // Parse the timestamp from the key
+
+              var millis = timestamp;
+              var dt = DateTime.fromMillisecondsSinceEpoch(millis * 1000);
+
+              // 12 Hour format:
+              var d12 =
+                  DateFormat('hh:mm a').format(dt); // 12/31/2000, 10:00 PM
+              print(millis);
+              print(d12);
+              dataList.add(SensorData(waterTemperature, pH, timestamp, d12));
+            }
+          });
+          // Sort the dataList based on the timestamp in ascending order
+          setState(() {
+            dataList.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+          });
+        }
+
+        // Update lastVal with the latest pH value
+        if (dataList.isNotEmpty) {
+          setState(() {
+            phVal = dataList[dataList.length - 1].pH.toString();
+          });
+        } else {
+          setState(() {
+            phVal = 'No data found.';
+          });
+          print("No data found.");
+        }
+      });
+    });
+  }
+
+  LineChartData createChartData() {
+    return LineChartData(
+      gridData: FlGridData(show: false),
+      titlesData: FlTitlesData(show: false),
+      borderData: FlBorderData(show: false),
+      minX: dataList.first.timestamp.toDouble(),
+      maxX: dataList.last.timestamp.toDouble(),
+      minY: 0,
+      maxY: 14, // Adjust this value according to your pH level range
+      lineBarsData: [
+        LineChartBarData(
+          spots: dataList.map((data) {
+            return FlSpot(data.timeUpload.toString(), data.pH);
+          }).toList(),
+          isCurved: true,
+          color: Colors.blue,
+          dotData: FlDotData(show: false),
+          belowBarData: BarAreaData(show: false),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // access the list of todos in the provider
@@ -44,114 +149,120 @@ class _PHPageState extends State<PH_Page> {
     //}
 
     return Scaffold(
-      drawer: Drawer(
-          child: ListView(padding: EdgeInsets.zero, children: [
-        SizedBox(height: 100),
-        ListTile(
-          title: const Text('Logout'),
-          onTap: () {
-            context.read<AuthProvider>().signOut();
-            Navigator.pushNamed(context, "/");
-          },
+        drawer: Drawer(
+            child: ListView(padding: EdgeInsets.zero, children: [
+          SizedBox(height: 100),
+          ListTile(
+            title: const Text('Logout'),
+            onTap: () {
+              context.read<AuthProvider>().signOut();
+              Navigator.pushNamed(context, "/");
+            },
+          ),
+        ])),
+        appBar: AppBar(
+          title: Text("PH Page"),
         ),
-      ])),
-      appBar: AppBar(
-        title: Text("PH Page"),
-      ),
-      body: Text("PH Page!"),
-      // body: StreamBuilder(
-      //   stream: todosStream,
-      //   builder: (context, snapshot) {
-      //     if (snapshot.hasError) {
-      //       return Center(
-      //         child: Text("Error encountered! ${snapshot.error}"),
-      //       );
-      //     } else if (snapshot.connectionState == ConnectionState.waiting) {
-      //       return Center(
-      //         child: CircularProgressIndicator(),
-      //       );
-      //     } else if (!snapshot.hasData) {
-      //       return Center(
-      //         child: Text("No Todos Found"),
-      //       );
-      //     }
+        body: Center(
+            child: Column(
+          children: [
+            Text("PH Level: $phVal"),
+            // Expanded(child: LineChart(createChartData()))
+          ],
+        ))
+        // body: StreamBuilder(
+        //   stream: todosStream,
+        //   builder: (context, snapshot) {
+        //     if (snapshot.hasError) {
+        //       return Center(
+        //         child: Text("Error encountered! ${snapshot.error}"),
+        //       );
+        //     } else if (snapshot.connectionState == ConnectionState.waiting) {
+        //       return Center(
+        //         child: CircularProgressIndicator(),
+        //       );
+        //     } else if (!snapshot.hasData) {
+        //       return Center(
+        //         child: Text("No Todos Found"),
+        //       );
+        //     }
 
-      //     return ListView.builder(
-      //       itemCount: snapshot.data?.docs.length,
-      //       itemBuilder: ((context, index) {
-      //         Todo todo = Todo.fromJson(
-      //             snapshot.data?.docs[index].data() as Map<String, dynamic>);
-      //         return Dismissible(
-      //           key: Key(todo.id.toString()),
-      //           onDismissed: (direction) {
-      //             context.read<TodoListProvider>().changeSelectedTodo(todo);
-      //             context.read<TodoListProvider>().deleteTodo();
+        //     return ListView.builder(
+        //       itemCount: snapshot.data?.docs.length,
+        //       itemBuilder: ((context, index) {
+        //         Todo todo = Todo.fromJson(
+        //             snapshot.data?.docs[index].data() as Map<String, dynamic>);
+        //         return Dismissible(
+        //           key: Key(todo.id.toString()),
+        //           onDismissed: (direction) {
+        //             context.read<TodoListProvider>().changeSelectedTodo(todo);
+        //             context.read<TodoListProvider>().deleteTodo();
 
-      //             ScaffoldMessenger.of(context).showSnackBar(
-      //                 SnackBar(content: Text('${todo.title} dismissed')));
-      //           },
-      //           background: Container(
-      //             color: Colors.red,
-      //             child: const Icon(Icons.delete),
-      //           ),
-      //           child: ListTile(
-      //             title: Text(todo.title),
-      //             leading: Checkbox(
-      //               value: todo.completed,
-      //               onChanged: (bool? value) {
-      //                 context
-      //                     .read<TodoListProvider>()
-      //                     .toggleStatus(index, value!);
-      //               },
-      //             ),
-      //             trailing: Row(
-      //               mainAxisSize: MainAxisSize.min,
-      //               children: [
-      //                 IconButton(
-      //                   onPressed: () {
-      //                     // showDialog(
-      //                     //   context: context,
-      //                     //   builder: (BuildContext context) => TodoModal(
-      //                     //     type: 'Edit',
-      //                     //     todoIndex: index,
-      //                     //   ),
-      //                     // );
-      //                   },
-      //                   icon: const Icon(Icons.create_outlined),
-      //                 ),
-      //                 IconButton(
-      //                   onPressed: () {
-      //                     context
-      //                         .read<TodoListProvider>()
-      //                         .changeSelectedTodo(todo);
-      //                     showDialog(
-      //                       context: context,
-      //                       builder: (BuildContext context) => TodoModal(
-      //                         type: 'Delete',
-      //                       ),
-      //                     );
-      //                   },
-      //                   icon: const Icon(Icons.delete_outlined),
-      //                 )
-      //               ],
-      //             ),
-      //           ),
-      //         );
-      //       }),
-      //     );
-      //   },
-      // ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) => TodoModal(
-              type: 'Add',
-            ),
-          );
-        },
-        child: const Icon(Icons.add_outlined),
-      ),
-    );
+        //             ScaffoldMessenger.of(context).showSnackBar(
+        //                 SnackBar(content: Text('${todo.title} dismissed')));
+        //           },
+        //           background: Container(
+        //             color: Colors.red,
+        //             child: const Icon(Icons.delete),
+        //           ),
+        //           child: ListTile(
+        //             title: Text(todo.title),
+        //             leading: Checkbox(
+        //               value: todo.completed,
+        //               onChanged: (bool? value) {
+        //                 context
+        //                     .read<TodoListProvider>()
+        //                     .toggleStatus(index, value!);
+        //               },
+        //             ),
+        //             trailing: Row(
+        //               mainAxisSize: MainAxisSize.min,
+        //               children: [
+        //                 IconButton(
+        //                   onPressed: () {
+        //                     // showDialog(
+        //                     //   context: context,
+        //                     //   builder: (BuildContext context) => TodoModal(
+        //                     //     type: 'Edit',
+        //                     //     todoIndex: index,
+        //                     //   ),
+        //                     // );
+        //                   },
+        //                   icon: const Icon(Icons.create_outlined),
+        //                 ),
+        //                 IconButton(
+        //                   onPressed: () {
+        //                     context
+        //                         .read<TodoListProvider>()
+        //                         .changeSelectedTodo(todo);
+        //                     showDialog(
+        //                       context: context,
+        //                       builder: (BuildContext context) => TodoModal(
+        //                         type: 'Delete',
+        //                       ),
+        //                     );
+        //                   },
+        //                   icon: const Icon(Icons.delete_outlined),
+        //                 )
+        //               ],
+        //             ),
+        //           ),
+        //         );
+        //       }),
+        //     );
+        //   },
+        // ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     showDialog(
+        //       context: context,
+        //       builder: (BuildContext context) => TodoModal(
+        //         type: 'Add',
+        //       ),
+        //     );
+        //   },
+        //   child: const Icon(Icons.add_outlined),
+        // ),
+        );
   }
 }
--- lib/screens/todo_page.dart
+++ lib/screens/todo_page.dart
@@ -6,6 +6,7 @@
 
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:week7_networking_discussion/models/todo_model.dart';
 import 'package:week7_networking_discussion/providers/todo_provider.dart';
@@ -32,11 +33,65 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _subscribeToSensorData();
+  // }
+
+  // void _subscribeToSensorData() async {
+  //   DateTime current_date = DateTime.now();
+  //   String date_today = current_date.toString().split(' ')[0];
+  //   final ref = FirebaseDatabase(
+  //       databaseURL:
+  //           "https://sp2-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
+  //   void streamSubscription = await ref
+  //       .reference()
+  //       .child("data_sensor/$date_today")
+  //       .onValue
+  //       .listen((DatabaseEvent event) {
+  //     // Update lastVal and trigger a rebuild
+  //     setState(() {
+  //       Map<dynamic, dynamic> sensorDataMap =
+  //           event.snapshot.value as Map<dynamic, dynamic>;
+  //       List<SensorData> dataList = [];
+  //       if (sensorDataMap != null) {
+  //         sensorDataMap.forEach((key, value) {
+  //           if (value != null) {
+  //             var waterTemperature = value["water_temperature"].toDouble();
+  //             var pH = value["pH"].toDouble();
+  //             var timestamp =
+  //                 int.parse(key); // Parse the timestamp from the key
+
+  //             dataList.add(SensorData(waterTemperature, pH, timestamp));
+  //           }
+  //         });
+
+  //         // Sort the dataList based on the timestamp in ascending order
+  //         dataList.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+  //       }
+
+  //       // Update lastVal with the latest pH value
+  //       if (dataList.isNotEmpty) {
+  //         setState(() {
+  //           lastVal = dataList[dataList.length - 1].pH.toString();
+  //         });
+
+  //         print("recent pH value: $lastVal");
+  //       } else {
+  //         lastVal = 'No data found.';
+  //         print("No data found.");
+  //       }
+  //     });
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     // access the list of todos in the provider
-    Stream<QuerySnapshot> todosStream = context.watch<TodoListProvider>().todos;
-
+    // Stream<QuerySnapshot> todosStream = context.watch<TodoListProvider>().todos;
+    // var lastVal = 'none';
     return Scaffold(
         drawer: Drawer(
             child: ListView(padding: EdgeInsets.zero, children: [
@@ -50,7 +105,7 @@ class _TodoPageState extends State<TodoPage> {
           ),
         ])),
         appBar: AppBar(
-          title: Text("Home Page"),
+          title: Text("AquaTrack"),
         ),
         body: Center(
           // Wrap the ListView with a Container and set the width
@@ -102,12 +157,12 @@ class _TodoPageState extends State<TodoPage> {
                     //   print('No data available.');
                     // }
 
-                    DateTime current_date = DateTime.now();
-                    String date_today = current_date.toString().split(' ')[0];
-                    // String date_today = "2023-08-03";
-                    final ref = FirebaseDatabase(
-                        databaseURL:
-                            "https://sp2-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/");
+                    // DateTime current_date = DateTime.now();
+                    // String date_today = current_date.toString().split(' ')[0];
+                    // // String date_today = "2023-08-03";
+                    // final ref = FirebaseDatabase(
+                    //     databaseURL:
+                    //         "https://sp2-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/");
                     // print(date_today);
                     // StreamSubscription<DatabaseEvent> stream = await ref
                     //     .reference()
@@ -125,47 +180,51 @@ class _TodoPageState extends State<TodoPage> {
                     // });
                     // print(stream);
 
-                    StreamSubscription<DatabaseEvent> stream = await ref
-                        .reference()
-                        .child("data_sensor/$date_today")
-                        .onValue
-                        .listen((DatabaseEvent event) {
-                      Map<dynamic, dynamic> sensorDataMap =
-                          event.snapshot.value as Map<dynamic, dynamic>;
-                      List<SensorData> dataList = [];
-                      print("no problems here");
-                      if (sensorDataMap != null) {
-                        sensorDataMap.forEach((key, value) {
-                          if (value != null) {
-                            var waterTemperature =
-                                value["water_temperature"].toDouble();
-                            var pH = value["pH"].toDouble();
-                            var timestamp = int.parse(
-                                key); // Parse the timestamp from the key
+                    // StreamSubscription<DatabaseEvent> stream = await ref
+                    //     .reference()
+                    //     .child("data_sensor/$date_today")
+                    //     .onValue
+                    //     .listen((DatabaseEvent event) {
+                    //   Map<dynamic, dynamic> sensorDataMap =
+                    //       event.snapshot.value as Map<dynamic, dynamic>;
+                    //   List<SensorData> dataList = [];
+                    //   print("no problems here");
+                    //   if (sensorDataMap != null) {
+                    //     sensorDataMap.forEach((key, value) {
+                    //       if (value != null) {
+                    //         var waterTemperature =
+                    //             value["water_temperature"].toDouble();
+                    //         var pH = value["pH"].toDouble();
+                    //         var timestamp = int.parse(
+                    //             key); // Parse the timestamp from the key
 
-                            dataList.add(
-                                SensorData(waterTemperature, pH, timestamp));
-                          }
-                        });
+                    //         dataList.add(
+                    //             SensorData(waterTemperature, pH, timestamp));
+                    //       }
+                    //     });
 
-                        // Sort the dataList based on the timestamp in ascending order
-                        dataList
-                            .sort((a, b) => a.timestamp.compareTo(b.timestamp));
-                      }
+                    //     // Sort the dataList based on the timestamp in ascending order
+                    //     dataList
+                    //         .sort((a, b) => a.timestamp.compareTo(b.timestamp));
+                    //   }
 
-                      // Print the data in ascending order of timestamp
-                      if (dataList.isNotEmpty) {
-                        print("Data in Ascending Order based on Timestamp:");
-                        for (var data in dataList) {
-                          print("Timestamp: ${data.timestamp}");
-                          print("Water Temperature: ${data.waterTemperature}");
-                          print("pH Value: ${data.pH}");
-                          print("");
-                        }
-                      } else {
-                        print("No data found.");
-                      }
-                    });
+                    // Print the data in ascending order of timestamp
+                    //   if (dataList.isNotEmpty) {
+                    //     print("Data in Ascending Order based on Timestamp:");
+                    //     for (var data in dataList) {
+                    //       print("Timestamp: ${data.timestamp}");
+                    //       print("Water Temperature: ${data.waterTemperature}");
+                    //       print("pH Value: ${data.pH}");
+                    //       print("");
+                    //     }
+                    //     setState(() {
+                    //       lastVal = dataList[dataList.length - 1].pH.toString();
+                    //       print("recent pH value: $lastVal");
+                    //     });
+                    //   } else {
+                    //     print("No data found.");
+                    //   }
+                    // });
 
                     // Map<Object?, Object?> sensor_data =
                     //     event.snapshot.value as Map<Object?, Object?>;
@@ -176,7 +235,7 @@ class _TodoPageState extends State<TodoPage> {
                     //   // Access the timestamp of the update
                     // });
                     // Stream<Qu
-                    //Navigator.pushNamed(context, "/phPage");
+                    Navigator.pushNamed(context, "/phPage");
 
                     // try {
                     //   await ref
